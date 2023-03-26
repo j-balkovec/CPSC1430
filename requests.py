4c16e32b3c6e4580f13134a1a61b39e04b71849a
j-balkovec <116223977+j-balkovec@users.noreply.github.com>
2023-03-25 18:18:14 -0700

    Delete requests.py

--- requests.py
+++
@@ -1,9 +0,0 @@
-import requests as req
-
-url = 'http://www.golfportal.si/'
-response = req.get(url)
-
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(f'Error accessing website. Status code: {response.status_code}')
